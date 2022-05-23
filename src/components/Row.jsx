2a43644c2fc78d5ecf7c6c2909825a11a8bdac83
Arushi Katyal <katy.arushi@gmail.com>
2022-05-22 23:52:52 -0400

    create row component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -10,14 +10,14 @@ const Row = ({ title, fetchURL }) => {
 
 	return (
 		<>
-			<h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-			<div className='relative flex items-center'>
+			<h2 className='text-white font-bold md:text-xl p-8'>{title}</h2>
+			<div className='relative flex items-center p-8'>
 				<div id={'slider'}>
 					{movies.map((item, id) => (
 						<div className='w-[160px] sm:w-[300px] md:w-[240px] lg:w-[200px] inline-block cursor-pointer relative p-2'>
 							<img
-								src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-								alt={movie?.title}
+								src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+								alt={item?.title}
 							/>
 						</div>
 					))}
