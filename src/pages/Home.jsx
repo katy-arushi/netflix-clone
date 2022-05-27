5eb55acbef50208af1566b0fabdfd63427d32989
Arushi Katyal <katy.arushi@gmail.com>
2022-05-27 17:01:26 -0400

    add row id for slider functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,16 @@ import Row from '../components/Row';
 import requests from '../requests';
 
 const Home = () => {
-  return (
-    <>
-      <Main />
-      <Row title='Upcoming' fetchURL={requests.requestUpcoming}/>
-      <Row title='Popular' fetchURL={requests.requestPopular}/>
-      <Row title='Trending' fetchURL={requests.requestTrending}/>
-      <Row title='Top Rated' fetchURL={requests.requestTopRated}/>
-      <Row title='Horror' fetchURL={requests.requestHorror}/>
-    </>
-  )
+	return (
+		<>
+			<Main />
+			<Row rowId='1' title='Upcoming' fetchURL={requests.requestUpcoming} />
+			<Row rowId='2' title='Popular' fetchURL={requests.requestPopular} />
+			<Row rowId='3' title='Trending' fetchURL={requests.requestTrending} />
+			<Row rowId='4' title='Top Rated' fetchURL={requests.requestTopRated} />
+			<Row rowId='5' title='Horror' fetchURL={requests.requestHorror} />
+		</>
+	);
 };
 
 export default Home;
