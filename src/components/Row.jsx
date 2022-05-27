c0c352a345adbd3ea2c3ac02702603b007e65930
Arushi Katyal <katy.arushi@gmail.com>
2022-05-27 16:59:58 -0400

    add slider icons and functionality for row component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,26 +1,46 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Movie from './Movie';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
 	const [movies, setMovies] = useState([]);
 
 	useEffect(() => {
 		axios.get(fetchURL).then(response => setMovies(response.data.results));
 	}, [fetchURL]);
 
+	const slideLeft = () => {
+		let slider = document.getElementById('slider' + rowID);
+		slider.scrollLeft = slider.scrollLeft - 500;
+	};
+	const slideRight = () => {
+		let slider = document.getElementById('slider' + rowID);
+		slider.scrollLeft = slider.scrollLeft + 500;
+	};
+
 	return (
 		<>
 			<h2 className='text-white font-bold md:text-xl p-8'>{title}</h2>
-			<div className='relative flex items-center p-8'>
+			<div className='relative flex items-center p-8 group'>
+				<MdChevronLeft
+					onClick={slideLeft}
+					size={40}
+					className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+				/>
 				<div
-					id={'slider'}
+					id={'slider' + rowID}
 					className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
 				>
 					{movies.map((item, id) => (
 						<Movie key={id} item={item} />
 					))}
 				</div>
+				<MdChevronRight
+					onClick={slideRight}
+					size={40}
+					className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+				/>
 			</div>
 		</>
 	);
